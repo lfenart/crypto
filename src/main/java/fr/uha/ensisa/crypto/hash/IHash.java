2771d297f5b0eea490437c1b14e34eb5b802c23f
lfenart <lucas.fenart@gmail.com>
2020-01-08 22:32:03 +0100

    Add javadoc for IHash methods

--- src/main/java/fr/uha/ensisa/crypto/hash/IHash.java
+++ src/main/java/fr/uha/ensisa/crypto/hash/IHash.java
@@ -9,12 +9,29 @@ package fr.uha.ensisa.crypto.hash;
 
 public interface IHash {
 
+	/**
+	 * Create the hash of the input set using setInput
+	 * @return hash of the input
+	 */
 	public byte[] createHash();
 	
+	/**
+	 * Verify that the parameter is the hash of the input
+	 * @param bytes the hash to verify
+	 * @return true if the hash is verified, false if not
+	 */
 	public boolean verify(byte[] bytes);
 
+	/**
+	 * Set the input
+	 * @param input input
+	 */
 	public void setInput(byte[] input);
-
+	
+	/**
+	 * Get the name of the IHash
+	 * @return name
+	 */
 	public String name();
 
 }
